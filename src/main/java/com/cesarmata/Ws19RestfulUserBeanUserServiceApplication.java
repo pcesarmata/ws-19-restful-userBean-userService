7537b053a2c406fe0ac5f63deb5dade0bd4f0ef1
Cesa <pcesarmata@gmail.com>
2021-03-21 21:06:34 -0600

    Init commit

---
+++ src/main/java/com/cesarmata/Ws19RestfulUserBeanUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws19RestfulUserBeanUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws19RestfulUserBeanUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarmata/Ws19RestfulUserBeanUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws19RestfulUserBeanUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
